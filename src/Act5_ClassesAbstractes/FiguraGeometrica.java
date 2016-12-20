b9d33d2428d58d16d96129a92c57cdb0f3a2aa9d
Antoni Rossello Daureo <tonirossellod@gmail.com>
2016-12-20 20:28:57 +0100

    update

---
+++ src/Act5_ClassesAbstractes/FiguraGeometrica.java
@@ -0,0 +1,52 @@
+package Act5_ClassesAbstractes;
+
+import java.awt.Color;
+
+/**
+ * Created by Budha on 30/09/16.
+ */
+
+public abstract class FiguraGeometrica {
+    protected int codi = 0;
+    protected String nom = "";
+    protected Color color = Color.white;
+
+    public FiguraGeometrica(int codi, String nom, Color color) {
+        this.codi = codi;
+        this.nom = nom;
+        this.color = color;
+    }
+
+    public void setCodi(int nouCodi) {
+        if (nouCodi >= 0) codi = nouCodi;
+    }
+
+    public void setNom(String nouNom) {
+        nom = nouNom;
+    }
+
+    public void setColor(Color nouColor) {
+        color = nouColor;
+    }
+
+    public int getCodi() {
+        return codi;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "FiguraGeometrica{" +
+                "codi=" + codi +
+                ", nom='" + nom + '\'' +
+                ", color=" + color +
+                '}';
+    }
+}
---
+++ src/Act5_ClassesAbstractes/Main.java
@@ -0,0 +1,11 @@
+package Act5_ClassesAbstractes;
+
+/**
+ * Created by Budha on 30/09/16.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
